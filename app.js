fa3eba2cecd1f280618c9c3c919e5f2045f7a8b6
Katie De Rosier <ktderosier@gmail.com>
2020-05-30 12:43:34 +1000

    appended the quiz to the DOM and added some logic for when an answer is selected

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ async function getDogs(dogSearch) {
   return dogResponse;
 }
 
+///append breed choices to dropdown menu
 async function selectBreed() {
   const dropDownResults = await $.ajax(
     `https://api.TheDogAPI.com/v1/breeds`
@@ -60,26 +61,8 @@ $(() => {
     console.log("dog image", dogImage);
 
     appendDogInfo(dogImage);
-  });
+});
 ///////QUIZ
-
-  const quizContainer = $("#quiz");
-  const resultsContainer = $("results");
-  const submitButton = $("submit");
-
-  function buildQuiz(){
-
-  }
-
-  function showResults(){
-
-  }
-  buildQuiz();
-
-  //for submit
-  $("#submitQuiz").on('click', showResults);
-
-
   const myQuestions = [
     {
       question: "What is your favorite Sunday activity?",
@@ -87,43 +70,50 @@ $(() => {
         a: "Sleep in and watch Netflix all day",
         b: "Wake up at 6am, go for a 15km run, and organize everything",
         c: "Read a book and contemplate the meaning of life/ investigate unsolved crimes"
-      },
+    }},
+    {
       question: "What is your favorite food?",
       answers: {
         a: "Chocolate, candy, chips, anything sweet or salty",
         b: "Grilled chicken and broccoli",
         c: "Caviar"
-      },
+    }},
+    {
       question: "What's your hobby?",
       answers: {
         a: "Is Netflix a hobby?",
         b: "Exercising",
         c: "Studying philosophy/ other deep subjects"
-      },
+    }},
+    {
       question: "If you could have any career in the world, what would it be?",
       answers: {
         a: "Food taster",
         b: "Own a global gym franchise",
         c: "Detective"
-      },
+    }},
+      {
       question: "How would your friends describe you?",
       answers: {
         a: "Chilled out, peaceful, and easygoing",
         b: "Active, organized, and happy",
         c: "Curious, imaginative, and loyal"
-      },
+      }},
+      {
       question: "Where do you want travel most?",
       answers: {
         a: "Austria, for the spas",
         b: "France, so I can finally take part in Tour de France and eat croissants",
         c: "Berlin, to wander around the city and look at art"
-      },
+      }},
+      {
       question: "What's most important to you?",
       answers: {
         a: "World and self peace",
         b: "Pushing myself to my max potential",
         c: "Continuous learning"
-      },
+      }},
+      {
       question: "What's your favorite drink?",
       answers: {
         a: "Beer",
@@ -132,4 +122,54 @@ $(() => {
       }
     }
   ]
+
+  const quizContainer = $("#quiz");
+  const resultsContainer =$("#results");
+  const submitButton = $("#submitQuiz");
+  let answers = {
+    A: 0,
+    B: 0,
+    C: 0
+  };
+
+  function buildQuiz(questionObject){
+    questionObject.map(function (quizQuestion, index){
+        // $("#quiz").children().remove();
+        $("#quiz").append(`<h1 id=${index}>${quizQuestion.question}</h1>`)
+        $("#quiz").append(`<h2><button id="A" class="quizButtons">${quizQuestion.answers.a}</h2></button>
+        <h2><button id="B" class="quizButtons">${quizQuestion.answers.b}</h2></button>
+        <h2><button id="C" class="quizButtons">${quizQuestion.answers.c}</h2></button>`) 
+        quizProgress()
+        console.log("quiz question", quizQuestion)
+    })
+      $(".quizButtons").on('click', (e) =>{
+
+    let selectedAnswer = answers[e.target.id];
+      answers[selectedAnswer] += 1;
+
+      console.log(answers);
+      
+      })
+    }
+
+//quiz progress function
+function quizProgress(){
+
+}
+
+//show results
+
+function showResults(){
+
+}
+  //display quiz
+
+  buildQuiz(myQuestions);
+
+  //for submit
+  $("#submitQuiz").on('click', showResults);
+
+
+
+
 });
